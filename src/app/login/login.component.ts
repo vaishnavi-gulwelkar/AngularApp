e5817406d5ce90652912d7c82bf965babf0be6b1
vaishnavi-gulwelkar <vaishnavigulwelkar629@gmail.com>
2021-02-06 09:49:53 -0800

    login and register with ui and validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,9 +8,44 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  email = '';
+  password = '';
+  istyping1 = true;
+  istyping2 = true;
+  isInvalid = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    console.warn('Clicked')
+
+    if (this.email == 'admin' && this.password == 'admin') {
+      console.warn('inside if')
+      alert('login success');
+    }
+    else if (this.email == '' && this.password == '') {
+      alert('please enter emaild id and password');
+    }
+    else if (this.email == '') {
+      alert('please enter email id')
+    }
+    else if (this.password == '') {
+      alert('please enter password');
+    }
+    else {
+      this.isInvalid = true;
+    }
+  }
+
+  typing1() {
+    console.warn('inside typing1 method')
+    this.istyping1 = false;
+  }
+  typing2() {
+    console.warn('inside typing1 method')
+    this.istyping2 = false;
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  email = '';
+  password = '';
+  repassword = '';
+  istyping1 = true;
+  istyping2 = true;
+  istyping3 = true;
+  isInvalid = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  register() {
+    console.warn('Clicked')
+
+    if (this.email == '' && this.password == '') {
+      alert('please enter emaild id and password');
+    }
+    else if (this.email == '') {
+      alert('please enter email id')
+    }
+    else if (this.password == '') {
+      alert('please enter password');
+    }
+    else if (this.repassword == '') {
+      alert('please enter password again');
+    }
+    else {
+      this.isInvalid = true;
+    }
+  }
+
+  typing1() {
+    console.warn('inside typing1 method')
+    this.istyping1 = false;
+  }
+  typing2() {
+    console.warn('inside typing1 method')
+    this.istyping2 = false;
+  }
+  typing3() {
+    console.warn('inside typing1 method')
+    this.istyping3 = false;
   }
 
 }
